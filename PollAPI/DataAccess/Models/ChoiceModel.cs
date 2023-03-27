c7436a63aa28d4b1adcdb6295dac0745e8c7356f
mir4H <mira.m.hamalainen@gmail.com>
2023-03-27 14:56:19 +0300

    Creating DataAccess+models

---
+++ PollAPI/DataAccess/Models/ChoiceModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class ChoiceModel
+    {
+        public int Choice_id { get; set; }
+        public string Text { get; set; } = "";
+        public int Votes { get; set; } = 0;
+        public int Poll_id { get; set; }
+    }
+}
---
+++ PollAPI/DataAccess/Models/PollModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class PollModel
+    {
+        public int Poll_id { get; set; }
+        public string Title { get; set; } = "";
+        public DateTime Created_at { get; set; }
+    }
+}
